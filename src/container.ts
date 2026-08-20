1f40c11b4cb87d290a12b6c8daab498e5122296e
Nhlalo <nhlalonkosi@gmail.com>
2026-08-20 14:07:11 +0200

    feat(container): add CaseService with proper dependency injection

--- src/container.ts
+++ src/container.ts
@@ -1,24 +1,33 @@
 import { AuthService } from "./services/AuthService";
 import { FuneralHomeService } from "./services/FuneralHomeService";
+import { CaseService } from "./services/CaseService";
 import { EmailService } from "./services/EmailService";
 import { EmailTemplateFactory } from "./templates/EmailTemplateFactory";
+import { FamilyLinkTemplate } from "./templates/FamilyLinkTemplate";
+import { DefaultLinkGenerator } from "./lib/DefaultLinkGenerator";
 import { UserRepository } from "./repositories/UserRepository";
 import { RefreshTokenRepository } from "./repositories/RefreshTokenRepository";
 import { FuneralHomeRepository } from "./repositories/FuneralHomeRepository";
 import { StaffRepository } from "./repositories/StaffRepository";
+import { CaseRepository } from "./repositories/CaseRepository";
 import { PrismaClient } from "../generated/prisma/client";
+import { RequestHandler } from "express";
+import { createAuthMiddleware } from "./middleware/auth.middleware";
 
 export class Container {
   private static instance: Container;
   private _authService?: AuthService;
   private _funeralHomeService?: FuneralHomeService;
+  private _caseService?: CaseService;
   private _emailService?: EmailService;
+  private _authMiddleware?: RequestHandler;
 
   constructor(
     private userRepo: UserRepository,
     private refreshTokenRepo: RefreshTokenRepository,
     private funeralHomeRepo: FuneralHomeRepository,
     private staffRepo: StaffRepository,
+    private caseRepo: CaseRepository,
   ) {}
 
   static getInstance(
@@ -26,6 +35,7 @@ export class Container {
     refreshTokenRepo: RefreshTokenRepository,
     funeralHomeRepo: FuneralHomeRepository,
     staffRepo: StaffRepository,
+    caseRepo: CaseRepository,
   ): Container {
     if (!Container.instance) {
       Container.instance = new Container(
@@ -33,9 +43,17 @@ export class Container {
         refreshTokenRepo,
         funeralHomeRepo,
         staffRepo,
+        caseRepo,
       );
     }
     return Container.instance;
+  }
+
+  get authMiddleware(): RequestHandler {
+    if (!this._authMiddleware) {
+      this._authMiddleware = createAuthMiddleware(this.authService);
+    }
+    return this._authMiddleware;
   }
 
   get emailService(): EmailService {
@@ -59,6 +77,7 @@ export class Container {
       this._authService = new AuthService(
         this.userRepo,
         this.refreshTokenRepo,
+        this.staffRepo,
         this.emailService,
         process.env.JWT_ACCESS_SECRET!,
         process.env.JWT_REFRESH_SECRET!,
@@ -80,6 +99,25 @@ export class Container {
     }
     return this._funeralHomeService;
   }
+
+  get caseService(): CaseService {
+    if (!this._caseService) {
+      const baseUrl = process.env.BASE_URL || "yourplatform.com";
+      const expiryDays = parseInt(process.env.LINK_EXPIRY_DAYS || "30");
+
+      const linkGenerator = new DefaultLinkGenerator(baseUrl, expiryDays);
+      const emailTemplate = new FamilyLinkTemplate();
+
+      this._caseService = new CaseService(
+        this.caseRepo,
+        this.funeralHomeRepo,
+        this.emailService,
+        linkGenerator,
+        emailTemplate,
+      );
+    }
+    return this._caseService;
+  }
 }
 
 export const createContainer = (prisma: PrismaClient): Container => {
@@ -87,6 +125,13 @@ export const createContainer = (prisma: PrismaClient): Container => {
   const refreshTokenRepo = new RefreshTokenRepository(prisma);
   const funeralHomeRepo = new FuneralHomeRepository(prisma);
   const staffRepo = new StaffRepository(prisma);
+  const caseRepo = new CaseRepository(prisma);
 
-  return new Container(userRepo, refreshTokenRepo, funeralHomeRepo, staffRepo);
+  return new Container(
+    userRepo,
+    refreshTokenRepo,
+    funeralHomeRepo,
+    staffRepo,
+    caseRepo,
+  );
 };
