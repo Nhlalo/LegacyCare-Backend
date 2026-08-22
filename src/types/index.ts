b660d0b19ffa48cf50a7e701cd25ad850c340924
Nhlalo <nhlalonkosi@gmail.com>
2026-08-22 13:39:38 +0200

    feat(types): add AuthRequest and StaffWithUser types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,17 @@
+import { Request } from "express";
+import { Staff, User } from "../../generated/prisma/client";
+
 export type Role = "OWNER" | "MANAGER" | "STAFF" | "LIMITED";
+
+export type StaffWithUser = Staff & {
+  user: Pick<User, "id" | "email" | "firstName" | "lastName"> | null;
+};
+
+export interface AuthRequest extends Request {
+  userId: string;
+  funeralHomeId: string;
+  staffRole: string;
+  userEmail: string;
+  userFirstName: string;
+  userLastName: string;
+}
